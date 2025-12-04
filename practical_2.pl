5149546cd2602f45ae1a7f1d7667a769d194b251
Avi <107690801+Avi4326@users.noreply.github.com>
2025-12-04 17:54:36 +0530

    Delete practical_2.pl

--- practical_2.pl
+++
@@ -1,6 +0,0 @@
-% 2. Write a PROLOG program to implement conc(L1, L2, L3) where L2 is the list to be appended with L1 to get the resulted list L3.
-
-conc([], L2, L2).
-
-conc([Head|Tail], L2, [Head|L3]) :-
-    conc(Tail, L2, L3).
