7c512d0ddfe2fa7d8ffa6f62025784d97934003f
Avi <107690801+Avi4326@users.noreply.github.com>
2025-12-04 17:55:06 +0530

    Delete practical_5.pl

--- practical_5.pl
+++
@@ -1,7 +0,0 @@
-% 5. Write a PROLOG program to implement max(X, Y, M) so that M is the maximum of two numbers X and Y.
-
-% If X is greater than or equal to Y, then X is the maximum.
-max(X, Y, X) :- X >= Y.
-
-% If Y is greater than X, then Y is the maximum.
-max(X, Y, Y) :- X < Y.
