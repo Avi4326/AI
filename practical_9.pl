a8b2eba174e5610f7a6af6b4a8229c7d3e5dfcbe
Avi <107690801+Avi4326@users.noreply.github.com>
2025-12-04 17:55:51 +0530

    Delete practical_9.pl

--- practical_9.pl
+++
@@ -1,9 +0,0 @@
-% 9. PROLOG program to implement multi (N1, N2, R) : where N1 and N2 denotes the numbers to be multiplied and R represents the result.
-
-multi(_, 0, 0).
-
-multi(N1, N2, R) :-
-    N2 > 0,
-    N_Dec is N2 - 1,
-    multi(N1, N_Dec, R_Temp),
-    R is N1 + R_Temp.
